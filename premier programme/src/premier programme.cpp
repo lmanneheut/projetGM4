f38f82b8802f6187daa2045e2f44f2c9a835a2db
lmanneheut <lea.manneheut@insa-rouen.fr>
2017-03-17 10:32:09 +0100

    Ajout d'une sortie terminal "première modification"

---
+++ premier programme/src/premier programme.cpp
@@ -0,0 +1,31 @@
+//============================================================================
+// Name        : premier.cpp
+// Author      : Léa Manneheut
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+using namespace std;
+
+
+/*
+int main() {
+	double nombre1;
+	double nombre2, somme;
+
+	cout << "Entrez le premier nombre" << endl;
+	cin >>nombre1;
+	cout << "Entrez le deuxième nombre" << endl;
+	cin >>nombre2;
+	cout << "Calcul en cours" << endl;
+	somme = nombre1+nombre2;
+	cout <<"La somme des 2 nombres est: ";
+	cout << somme;
+
+	return 0;
+}
+*/
